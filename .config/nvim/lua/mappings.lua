d905c8593dd6d8590cf420a38bd3915186da00f7
Chris Engebretson <chris@mob.net>
2025-09-09 22:17:31 -0500

    mapping to toggle transparency

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,4 +7,10 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- toggle Transparency
+map(
+  "n",
+  "<leader>tt",
+  ":lua require('base46').toggle_transparency()<CR>",
+  { noremap = true, silent = true, desc = "Toggle Background Transparency" }
+)
