94438bff5386fd1b7bf2979a69836bb6ff2872fb
Chris Engebretson <chris@mob.net>
2025-09-12 14:53:20 -0500

    Add setup script to call to install dotfiles

---
+++ .config/setup.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# To use
+# curl https://raw.githubusercontent.com/cengebretson/dotfiles/master/.config/setup.sh | bash
+
+# define config alias locally since the dotfiles
+# aren't installed on the system yet
+function dotfiles {
+   git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+# clone repo
+git clone --bare git@github.com:mrjones2014/dotfiles.git $HOME/.dotfiles
+dotfiles config status.showUntrackedFiles no
+
+# create a directory to backup existing dotfiles to
+mkdir -p .dotfiles-backup
+dotfiles checkout
+
+if [ $? = 0 ]; then
+  echo "Checked out dotfiles from git@github.com:mrjones2014/dotfiles.git";
+else
+  echo "Moving existing dotfiles to ~/.dotfiles-backup";
+  dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles-backup/{}
+  dotfiles checkout
+fi
+
