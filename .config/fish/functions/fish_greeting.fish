dc29a3de9ac26f7cbb487291be37225203cf0a66
Chris Engebretson <chris@mob.net>
2025-09-16 09:26:06 -0500

    Random image for neofetch display

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,3 +1,17 @@
 function fish_greeting
+    select_random_image
     neofetch
+end
+
+function select_random_image
+
+  # Get all files in the directory
+  set files (ls ~/.config/neofetch/option*)
+
+  # Pick a random file
+  set count (count $files)
+  set rand (random 1 $count)
+  set selected $files[$rand]
+
+  cp $selected ~/.config/neofetch/ascii.txt
 end
