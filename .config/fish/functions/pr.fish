4debf1e9072d76bba6d6b7fb94abc55035c10570
Chris Engebretson <chris@mob.net>
2026-07-09 11:46:55 -0500

    fish: new doctor command to diagnos issues

--- .config/fish/functions/pr.fish
+++
@@ -1,6 +0,0 @@
-function pr --description "Open the GitHub PR for the current branch in the browser"
-    if not gh pr view --web 2>/dev/null
-        echo "No open PR for this branch" >&2
-        return 1
-    end
-end
