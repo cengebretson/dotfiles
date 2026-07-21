1494bcdbf73678877bbc6287f50d7535dbab353c
Chris Engebretson <chris@mob.net>
2026-07-20 20:42:04 -0500

    fish: improved greeting script

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,21 +1,31 @@
 function fish_greeting
-    _select_random_image
-    fastfetch
+    if test (random 0 1) -eq 0
+        fastfetch
+    else
+        _display_random_iroh
+    end
 end
 
-function _select_random_image
-    set -l fastfetch_dir ~/.config/fastfetch
-    if not test -d "$fastfetch_dir"
+function _display_random_iroh
+    set -l iroh_dir ~/.config/nvim-v12/assets
+    if not test -d "$iroh_dir"
+        fastfetch
         return
     end
 
-    set -l files (command find "$fastfetch_dir" -type f -name 'option*' -print0 2>/dev/null | string split0)
+    set -l files (command find "$iroh_dir" -maxdepth 1 -type f -name 'iroh*.png' -print0 2>/dev/null | string split0)
     if test (count $files) -eq 0
+        fastfetch
         return
     end
 
     set -l rand (random 1 (count $files))
     set -l selected $files[$rand]
+    set -l width 96
 
-    cp -- "$selected" "$fastfetch_dir/ascii.txt"
+    if set -q COLUMNS; and test "$COLUMNS" -lt $width
+        set width $COLUMNS
+    end
+
+    chafa --size "$width"x24 --align top,left "$selected"
 end
